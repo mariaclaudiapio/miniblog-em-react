d6203f60d5b5183cf49a4b5a9d2e96863e14da88
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-01-01 13:48:53 -0300

    initial setup

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuvBVY5XBzSU-5dxpkbX7lT_llSbCgP_U",
+  authDomain: "miniblog-e1ea0.firebaseapp.com",
+  projectId: "miniblog-e1ea0",
+  storageBucket: "miniblog-e1ea0.appspot.com",
+  messagingSenderId: "884341031382",
+  appId: "1:884341031382:web:eac1919add54db377be477"
+}
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export { db }
