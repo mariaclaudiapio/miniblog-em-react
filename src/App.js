bd246f1804216c308bb3f6dd4ac2dcfe921c5e3b
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-01-01 16:47:41 -0300

    defining a general style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import Login from './pages/login/Login'
+import Register from './pages/Register/Register'
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
             <Routes>
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
             </Routes>        
           </div>
         <Footer />
