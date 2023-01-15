2cf40a02c5123b98ecadb880e13f492d28504e03
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-01-15 00:04:39 -0300

    montando a estrutura da página de cadastro

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
             </Routes>
           </div>
         <Footer />
